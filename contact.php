1d8bc008f61c2a3d6a73011e501e8102564fab2c
JaeNinja <jgardner21@student.gsu.edu>
2015-07-31 20:11:39 -0400

    Update
    Home Page and Contact Page

---
+++ contact.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Jae' Gardner</title>
+  <link rel="stylesheet" href="Index_Css.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style>
+body{
+	background-image: url("graphics/BG.jpg");
+}
+</style>
+</head>
+<body>
+<div class="center" >
+  <img src="graphics/JaeLogo.png" alt="Jae Gardner" width="460" height="300">     
+</div>
+
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+     
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.html">Home</a></li>
+        <!--<li><a href="graphics.html">Graphics/ Videos</a></li>-->
+         <li><a href="http://codd.cs.gsu.edu/~jgardner21/Recipe_index.html">Sample Site 1</a></li>
+        
+		<li><a href="contact.php">Contact Me</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<br><br>
+
+
+</body>
+</html>
